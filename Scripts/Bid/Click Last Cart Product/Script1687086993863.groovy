2ed4926abfdaaefc073f49301f72166d301531c4
alexdkm <intel7data@gmail.com>
2023-06-18 18:25:19 +0700

    Bid With Invalid Harga done

--- Scripts/Bid/Click Last Cart Product/Script1687086993863.groovy
+++ Scripts/Bid/Click Last Cart Product/Script1687086993863.groovy
@@ -17,5 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.click(findTestObject('Products/Product Detail Page/input_Harga Tawar'))
+WebUI.click(findTestObject('HomePage/a_Last Product Card'))
 
