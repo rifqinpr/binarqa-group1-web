daa43c4781393b42969b8205a3fd07fecd203ed3
alexdkm <intel7data@gmail.com>
2023-07-01 00:26:49 +0700

    Update Test Suites non gherkin invalid bid test case

--- Scripts/Step Definition/Feature Bid/BID05 - Bid with Null Price/Script1688143127519.groovy
+++ Scripts/Step Definition/Feature Bid/BID05 - Bid with Null Price/Script1688143127519.groovy
@@ -19,7 +19,7 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.callTestCase(findTestCase('Pages/Login/Click Masuk from homepage'), [:], FailureHandling.STOP_ON_FAILURE)
 
-WebUI.callTestCase(findTestCase('Pages/Login/Input Email'), [('email') : 'alexbeli1@gmail.com'], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('Pages/Login/Input Email'), [('email') : 'alexbeli3@gmail.com'], FailureHandling.STOP_ON_FAILURE)
 
 WebUI.callTestCase(findTestCase('Pages/Login/Input Password'), [('password') : 'asdqwe'], FailureHandling.STOP_ON_FAILURE)
 
