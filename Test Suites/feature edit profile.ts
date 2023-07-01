23758d1aca496f997a752ba4bb0adc907340c166
deksaplay <syafradha@gmail.com>
2023-07-01 15:18:03 +0700

    fddsfds

--- Test Suites/feature edit profile.ts
+++ Test Suites/feature edit profile.ts
@@ -11,4 +11,32 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>df810a22-ee93-43b0-8540-b391e20d84bb</testSuiteGuid>
+   <testCaseLink>
+      <guid>18ccfc32-fd11-48c4-b911-43f3b9d6045b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature User Profile/TC2-EDIT PROFILE WITH JPG</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ebf73fac-21f8-4381-ae6e-23aca772b032</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature User Profile/TC3-Edit profile without nama</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a1c868c2-7334-4511-b14a-6457ab78c909</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature User Profile/TC4-edit profile without no handphone</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fe40f1ed-68bb-4163-9a0d-8bb168c46548</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature User Profile/TC5-EDIT PROFILE WITHOUT ALAMAT</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
